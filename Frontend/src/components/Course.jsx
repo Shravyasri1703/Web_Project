4b7376ce52ff0133615fb1db7d9ff6697b007e8a
Shreyansh Mishra <shreyanshmishracr7@gmail.com>
2024-05-17 18:54:37 +0530

    Theme Switching

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -7,7 +7,7 @@ const Course = () => {
   return (
     <>
       <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
-        <div className="mt-28 text-center justify-center items-center">
+        <div className="mt-16 text-center justify-center items-center">
           <h1 className="text-2xl md:text-4xl">
             We're delighted to have you{" "}
             <span className="text-pink-500">here :)</span>
